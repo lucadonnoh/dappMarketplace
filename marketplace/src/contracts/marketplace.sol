6d0062112f7ae6ead8575c66b5e7206181a85343
Luca Donno <30298476+lucadonnoh@users.noreply.github.com>
2020-03-07 11:45:36 +0100

    Product struct

--- marketplace/src/contracts/marketplace.sol
+++ marketplace/src/contracts/marketplace.sol
@@ -4,6 +4,15 @@ contract Marketplace
 {
     string public name;
 
+    struct Product
+    {
+        uint id;
+        string name;
+        uint price;
+        address owner;
+        bool purchased;
+    }
+
     constructor() public
     {
         name = "Donnoh's Marketplace";
