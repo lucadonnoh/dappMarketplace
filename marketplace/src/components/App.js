83ad3077344e7aa868c01ac42b86ba2771a34906
Luca Donno <30298476+lucadonnoh@users.noreply.github.com>
2020-03-07 15:33:09 +0100

    navbar refactoring (component)

--- marketplace/src/components/App.js
+++ marketplace/src/components/App.js
@@ -3,7 +3,7 @@ import Web3 from 'web3'
 import logo from '../logo.png';
 import './App.css';
 import Marketplace from '../abis/Marketplace.json'
-
+import Navbar from './Navbar'
 class App extends Component {
 
   async componentWillMount() {
@@ -57,17 +57,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Donnoh's Marketplace
-          </a>
-          <p>{this.state.account}</p>
-        </nav>
+        <Navbar account={this.state.account}/>
         <div className="container-fluid mt-5">
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
