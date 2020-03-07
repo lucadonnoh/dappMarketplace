733f82c8557bb4b94c2ac54e18704dcd5a7d216e
Luca Donno <30298476+lucadonnoh@users.noreply.github.com>
2020-03-07 13:28:47 +0100

    added the ability to buy products + tests

--- marketplace/src/contracts/marketplace.sol
+++ marketplace/src/contracts/marketplace.sol
@@ -11,7 +11,7 @@ contract Marketplace
         uint id;
         string name;
         uint price;
-        address owner;
+        address payable owner;
         bool purchased;
     }
 
@@ -19,7 +19,15 @@ contract Marketplace
         uint id,
         string name,
         uint price,
-        address owner,
+        address payable owner,
+        bool purchased
+    );
+
+    event ProductPurchased(
+        uint id,
+        string name,
+        uint price,
+        address payable owner,
         bool purchased
     );
 
@@ -34,13 +42,39 @@ contract Marketplace
         require(bytes(_name).length > 0);
         //require a valid price
         require(_price > 0);
-        //make sure params are correct
         // increment productCount
         productCount++;
         // create the product
         products[productCount] = Product(productCount, _name, _price, msg.sender, false);
         // trigger an event
         emit ProductCreated(productCount, _name, _price, msg.sender, false);
+    }
+
+    function purchaseProduct(uint _id) public payable
+    {
+        // fetch product
+        Product memory _product = products[_id];
+        // fetch owner
+        address payable _seller = _product.owner;
+        // make sure the product has a valid id
+        require(_product.id > 0 && _product.id <= productCount);
+        // require that there is enough ether in the transaction
+        require(msg.value >= _product.price);
+        // require that the product has not been already purchased
+        require(!_product.purchased);
+        //require that the buyer is not the seller
+        require(_seller != msg.sender);
+        // transer the ownership to the buyer
+        _product.owner = msg.sender;
+        // mark as purchased
+        _product.purchased = true;
+        // update the product
+        products[_id] = _product;
+        // pay the seller by sending them ether
+        payable(address(_seller)).transfer(msg.value);
+        // trigger an event
+        emit ProductPurchased(productCount, _product.name, _product.price, msg.sender, true);
+
     }
 }
 
--- marketplace/test/Marketplace.test.js
+++ marketplace/test/Marketplace.test.js
@@ -48,11 +48,59 @@ contract('Marketplace', ([deployer, seller, buyer]) => {
             assert.equal(event.purchased, false, 'pruchased is correct')
 
             // FAILURE: product must have a name
-            await marketplace.createProduct('', web3.utils.toWei('1', 'Ether'), { from: seller }).should.be.rejected;
+            await await marketplace.createProduct('', web3.utils.toWei('1', 'Ether'), { from: seller }).should.be.rejected;
 
             // FAILURE: product must have a price
-            await marketplace.createProduct('Fortnite Ninja Skin', 0, { from: seller }).should.be.rejected;
+            await await marketplace.createProduct('Fortnite Ninja Skin', 0, { from: seller }).should.be.rejected;
+        })
 
+        it('lists products', async () => {
+            const product = await marketplace.products(productCount)
+            assert.equal(product.id.toNumber(), productCount.toNumber(), 'id is correct')
+            assert.equal(product.name, 'Fortnite Ninja Skin', 'name is correct')
+            assert.equal(product.price, '1000000000000000000', 'price is correct')
+            assert.equal(product.owner, seller, 'owner is correct')
+            assert.equal(product.purchased, false, 'pruchased is correct')
+        })
+
+        it('sells products', async () => {
+            // track seller balance before purchase
+            let oldSellerBalance
+            oldSellerBalance = await web3.eth.getBalance(seller)
+            oldSellerBalance = new web3.utils.BN(oldSellerBalance)
+
+            // SUCCESS: buyer makes purchase
+            result = await marketplace.purchaseProduct(productCount, { from: buyer, value: web3.utils.toWei('1', 'Ether') })
+
+            // check logs
+            const event = result.logs[0].args
+            assert.equal(event.id.toNumber(), productCount.toNumber(), 'id is correct')
+            assert.equal(event.name, 'Fortnite Ninja Skin', 'name is correct')
+            assert.equal(event.price, '1000000000000000000', 'price is correct')
+            assert.equal(event.owner, buyer, 'owner is correct')
+            assert.equal(event.purchased, true, 'purchased is correct')
+
+            // check that seller received funds
+            let newSellerBalance
+            newSellerBalance = await web3.eth.getBalance(seller)
+            newSellerBalance = new web3.utils.BN(newSellerBalance)
+
+            let price
+            price = web3.utils.toWei('1', 'Ether')
+            price = new web3.utils.BN(price)
+
+            const expectedBalance = oldSellerBalance.add(price)
+
+            assert.equal(newSellerBalance.toString(), expectedBalance.toString())
+
+            // FAILURE: tries to buy a aproduct that does not exist, i.e. product must have a valid id
+            await marketplace.purchaseProduct(99, { from: buyer, value: web3.utils.toWei('1', 'Ether') }).should.be.rejected;
+            // FAILURE: not enough ether
+            await marketplace.purchaseProduct(productCount, { from: buyer, value: web3.utils.toWei('0.5', 'Ether') }).should.be.rejected;
+            // FAILURE: deployer tries to buy the product, i.e. product can't be purchased twice
+            await marketplace.purchaseProduct(productCount, { from: deployer, value: web3.utils.toWei('1', 'Ether') }).should.be.rejected;
+            // FAILURE: buyer tries to buy the product, i.e. buyer can't be the seller
+            await marketplace.purchaseProduct(productCount, { from: seller, value: web3.utils.toWei('1', 'Ether') }).should.be.rejected;
         })
     })
 } )
